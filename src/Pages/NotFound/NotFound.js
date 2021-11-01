42bbc7607aab1b32f1a6b6f61fe790bd9f3e49de
Mahfuz Iqram <mahfujsunny.info@gmail.com>
2021-11-01 23:37:38 +0600

    complete

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-            
+            <h1 className="notfound">404 <small>NOt Found !!!</small></h1>
         </div>
     );
 };
